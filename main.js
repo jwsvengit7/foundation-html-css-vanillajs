bb7c9b801d36f4f61a836eb1be8d4cd315d91528
decagon <chiorlujack@gmail.com>
2023-09-06 03:58:32 +0100

    done

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 
+const change = document.querySelector("#change");
+let x=0;
+setInterval(()=>{
+    x++
+   if(x==1){
+    change.style.color="red"
+   } 
+   else if(x==2){
+    change.style.color="blue"
+   }
+   else if(x==3){
+    change.style.color="green"
+   }
+   else if(x==4){
+    change.style.color="pink"
+    x=0
+   }
+  
+ 
+   
+
+
+},1000)
